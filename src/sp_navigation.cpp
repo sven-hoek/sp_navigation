9c23be43eaf30888e52a17556011437acdaa8d57
Marcus Ebner von Eschenbach <herrlauch@googlemail.com>
2015-10-27 17:40:49 +0100

    Change naming convention of variables from 'my_variable' to 'myVariable'

--- src/sp_navigation.cpp
+++ src/sp_navigation.cpp
@@ -21,18 +21,18 @@ class StereoSubscriber
 {
 	private:
 	image_transport::ImageTransport it_;
-	image_transport::SubscriberFilter limage_sub_; /**< Left image msg. */
-	image_transport::SubscriberFilter rimage_sub_; /**< right image msg. */
-	message_filters::Subscriber<sensor_msgs::CameraInfo> lcinfo_sub_; /**< Left camera info msg. */
-	message_filters::Subscriber<sensor_msgs::CameraInfo> rcinfo_sub_; /**< Right camera info msg. */
+	image_transport::SubscriberFilter imgSub_l_; /**< Left image msg. */
+	image_transport::SubscriberFilter imgSub_r_; /**< right image msg. */
+	message_filters::Subscriber<sensor_msgs::CameraInfo> camInfoSub_l_; /**< Left camera info msg. */
+	message_filters::Subscriber<sensor_msgs::CameraInfo> camInfoSub_r_; /**< Right camera info msg. */
 	message_filters::Synchronizer<SyncPolicy> sync_; /**< Stereo topic synchronizer. */
-	image_transport::Publisher limage_pub_; /**< Left image publisher. */
-	image_transport::Publisher rimage_pub_; /**< Right image publisher. */
+	image_transport::Publisher imgPub_l_; /**< Left image publisher. */
+	image_transport::Publisher imgPub_r_; /**< Right image publisher. */
 	
 	public:
-	image_geometry::StereoCameraModel cam_model_;
-	cv_bridge::CvImageConstPtr limage_cptr_;
-	cv_bridge::CvImageConstPtr rimage_cptr_;
+	image_geometry::StereoCameraModel stCamModel_;
+	cv_bridge::CvImageConstPtr imgConstPtr_l_;
+	cv_bridge::CvImageConstPtr imgConstPtr_r_;
 
 
 	/* Constructor
@@ -42,50 +42,70 @@ class StereoSubscriber
 		it_(nh),
 		sync_(3)
 	{
-	    std::string stereo_namespace;
-	    stereo_namespace = nh.resolveName(nameToResolve);
+		std::string stereoNamespace;
+		stereoNamespace = nh.resolveName(nameToResolve);
 
-	    // Subscribe to stereo cameras
-	    limage_sub_.subscribe(it_, ros::names::clean(stereo_namespace + "/left/image_rect"), 3);
-	    rimage_sub_.subscribe(it_, ros::names::clean(stereo_namespace + "/right/image_rect"), 3);
-	    lcinfo_sub_.subscribe(nh, ros::names::clean(stereo_namespace + "/left/camera_info"), 3);
-	    rcinfo_sub_.subscribe(nh, ros::names::clean(stereo_namespace + "/right/camera_info"), 3);
-	    sync_.connectInput(limage_sub_, rimage_sub_, lcinfo_sub_, rcinfo_sub_),
-	    sync_.registerCallback(boost::bind(&StereoSubscriber::syncCallback, this, _1, _2, _3, _4));
-	    
-	    // Initialize publishers
-	    limage_pub_ = it_.advertise("sp_navigation/left/image_mono", 1);
-	    rimage_pub_ = it_.advertise("sp_navigation/right/image_mono", 1);
+		// Subscribe to stereo cameras
+		imgSub_l_.subscribe(it_, ros::names::clean(stereoNamespace + "/left/image_rect"), 3);
+		imgSub_r_.subscribe(it_, ros::names::clean(stereoNamespace + "/right/image_rect"), 3);
+		camInfoSub_l_.subscribe(nh, ros::names::clean(stereoNamespace + "/left/camera_info"), 3);
+		camInfoSub_r_.subscribe(nh, ros::names::clean(stereoNamespace + "/right/camera_info"), 3);
+		sync_.connectInput(imgSub_l_, imgSub_r_, camInfoSub_l_, camInfoSub_r_),
+		sync_.registerCallback(boost::bind(&StereoSubscriber::syncCallback, this, _1, _2, _3, _4));
+		
+		// Initialize publishers
+		imgPub_l_ = it_.advertise("sp_navigation/left/image_mono", 1);
+		imgPub_r_ = it_.advertise("sp_navigation/right/image_mono", 1);
 	}
 
 	/* Callback method for synchronized stereo images
 	 *
 	 * */
-	void syncCallback(const sensor_msgs::Image::ConstPtr& limage, const sensor_msgs::Image::ConstPtr& rimage, const sensor_msgs::CameraInfo::ConstPtr& lcinfo, const sensor_msgs::CameraInfo::ConstPtr& rcinfo)
+	void syncCallback(const sensor_msgs::Image::ConstPtr& lImage, const sensor_msgs::Image::ConstPtr& rImage, const sensor_msgs::CameraInfo::ConstPtr& lCamInfo, const sensor_msgs::CameraInfo::ConstPtr& rCamInfo)
 	{
-	  ROS_INFO_STREAM_NAMED("Subscriber Test", "got callback.");
-	  try
-	  {
-		cam_model_.fromCameraInfo(lcinfo, rcinfo);
-		limage_cptr_ = cv_bridge::toCvShare(limage, "mono8");
-		rimage_cptr_ = cv_bridge::toCvShare(rimage, "mono8");
-	  }
-	  catch (...)
-	  {
-	    ROS_ERROR("Error in syncCallback!");
-	  }
+		ROS_INFO_STREAM_NAMED("Subscriber Test", "got callback.");
+		try
+		{
+			stCamModel_.fromCameraInfo(lCamInfo, rCamInfo);
+			imgConstPtr_l_ = cv_bridge::toCvShare(lImage, "mono8");
+			imgConstPtr_r_ = cv_bridge::toCvShare(rImage, "mono8");
+		}
+		catch (cv_bridge::Exception& e)
+		{
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+		catch (...)
+		{
+			ROS_ERROR("Error in syncCallback!");
+			return;
+		}
 	}
+
 	
 	/* Simple method to publish cv::Mat images
 	 *
 	 * */
-	void publishCVImages(const cv::Mat& limage, const cv::Mat& rimage)
+	void publishCVImages(const cv::Mat& lImage, const cv::Mat& rImage)
 	{
-		cv_bridge::CvImage cvImLeft(std_msgs::Header(), "mono8", limage);
-		cv_bridge::CvImage cvImRight(std_msgs::Header(), "mono8", rimage);
-		limage_pub_.publish(cvImLeft.toImageMsg());
-		rimage_pub_.publish(cvImRight.toImageMsg());
+		cv_bridge::CvImage cvImLeft(std_msgs::Header(), "mono8", lImage);
+		cv_bridge::CvImage cvImRight(std_msgs::Header(), "mono8", rImage);
+		imgPub_l_.publish(cvImLeft.toImageMsg());
+		imgPub_r_.publish(cvImRight.toImageMsg());
 	}
+};
+
+
+class VisualOdometry
+{
+	private:
+		
+	
+	public:
+		void getImagePair(const StereoSubscriber& stereoSub)
+		{
+			
+		}
 };
 
 } //End of namespace
@@ -95,27 +115,28 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "sp_navigation");
 	ros::NodeHandle nh;
-	
-	sp_navigation::StereoSubscriber stereo_sub(nh, "stereo");
+	sp_navigation::StereoSubscriber stereoSub(nh, "stereo");
 
 	cv::Mat left, right;
+	cv::ORB oFDDE;	// ORB feature detector and descriptor extractor
+	cv::BFMatcher bfMatcher(cv::NORM_HAMMING, true); // Bruteforce matcher with implemented cross-checking if second arg is 'true'
 	
-	ros::Rate loop_rate(10);
+	ros::Rate loopRate(10);
 	while (ros::ok())
 	{
 		ros::spinOnce();
-		if (stereo_sub.limage_cptr_ != NULL && stereo_sub.rimage_cptr_ != NULL)
+		if (stereoSub.imgConstPtr_l_ != NULL && stereoSub.imgConstPtr_r_ != NULL)
 		{
-			stereo_sub.limage_cptr_->image.copyTo(left);
-			stereo_sub.rimage_cptr_->image.copyTo(right);
+			stereoSub.imgConstPtr_l_->image.copyTo(left);
+			stereoSub.imgConstPtr_r_->image.copyTo(right);
 			
-
 			
-			stereo_sub.publishCVImages(left, right);
+			
+			stereoSub.publishCVImages(left, right);
 		}
 		
 		
-		loop_rate.sleep();
+		loopRate.sleep();
 	}
 	
 	ros::spin();
