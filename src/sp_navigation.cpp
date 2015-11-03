8c15eb356a311bd921195f36eaae05a6c686e98b
Marcus Ebner von Eschenbach <herrlauch@googlemail.com>
2015-11-03 18:11:40 +0100

    Put all the visual odometry computing into class VisualOdometer. Take node out of launch file for testing.

--- src/sp_navigation.cpp
+++ src/sp_navigation.cpp
@@ -12,6 +12,7 @@
 #include <sensor_msgs/Image.h>
 #include <tf/transform_broadcaster.h>
 #include <image_geometry/stereo_camera_model.h>
+#include <stdexcept>
 
 namespace sp_navigation
 {
@@ -33,6 +34,7 @@ class StereoSubscriber
 		image_geometry::StereoCameraModel stCamModel_;
 		cv_bridge::CvImageConstPtr imgConstPtr_l_;
 		cv_bridge::CvImageConstPtr imgConstPtr_r_;
+		unsigned int imgIdx;
 
 
 		/* Constructor
@@ -40,7 +42,8 @@ class StereoSubscriber
 		 * */
 		StereoSubscriber(ros::NodeHandle& nh, const std::string nameToResolve) :
 			it_(nh),
-			sync_(3)
+			sync_(3),
+			imgIdx(0)
 			{
 			std::string stereoNamespace;
 			stereoNamespace = nh.resolveName(nameToResolve);
@@ -69,15 +72,16 @@ class StereoSubscriber
 				stCamModel_.fromCameraInfo(lCamInfo, rCamInfo);
 				imgConstPtr_l_ = cv_bridge::toCvShare(lImage, "bgr8");
 				imgConstPtr_r_ = cv_bridge::toCvShare(rImage, "bgr8");
+				imgIdx++;
 				}
-			catch (cv_bridge::Exception& e)
+			catch(cv_bridge::Exception& e)
 				{
 				ROS_ERROR("cv_bridge exception: %s", e.what());
 				return;
 				}
-			catch (...)
+			catch(...)
 				{
-				ROS_ERROR("Error in syncCallback!");
+				ROS_ERROR("Error in StereoSubscriber::syncCallback!");
 				return;
 				}
 			}
@@ -88,23 +92,219 @@ class StereoSubscriber
 		 * */
 		void publishCVImages(const cv::Mat& lImage, const cv::Mat& rImage)
 			{
-			cv_bridge::CvImage cvImLeft(std_msgs::Header(), "bgr8", lImage);
-			cv_bridge::CvImage cvImRight(std_msgs::Header(), "bgr8", rImage);
-			imgPub_l_.publish(cvImLeft.toImageMsg());
-			imgPub_r_.publish(cvImRight.toImageMsg());
+			try
+				{
+				cv_bridge::CvImage cvImLeft(std_msgs::Header(), "bgr8", lImage);
+				cv_bridge::CvImage cvImRight(std_msgs::Header(), "bgr8", rImage);
+				imgPub_l_.publish(cvImLeft.toImageMsg());
+				imgPub_r_.publish(cvImRight.toImageMsg());
+				}
+			catch(cv::Exception &e)
+				{
+				std::cout << e.what() << std::endl;
+				return;
+				}
+			catch(...)
+				{
+				ROS_ERROR("Error in StereoSubscriber::publishCVImages!");
+				}
 			}
 	};
 
-
-class VisualOdometry
+// Class for computing odometry from stereo images
+class VisualOdometer
 	{
 	private:
-		
+		cv::ORB oFDDE_;	// ORB feature detector and descriptor extractor
+		cv::BFMatcher bfMatcher_; // Bruteforce matcher with cross-checking of matches
+		StereoSubscriber* stereoSub_ = NULL;
+		std::string parentFrame_, childFrame_;
+		unsigned int lastImgIdx_;
 	
 	public:
-		void getImagePair(const StereoSubscriber& stereoSub)
+		cv::Mat imgPrev_l_, imgPrev_r_;
+		cv::Mat imgCurr_l_, imgCurr_r_;
+		// Keypoints/points in 'previous' images
+		std::vector<cv::KeyPoint> keyPointsPrev_l_, keyPointsPrev_r_;
+		std::vector<cv::Point2f> pointsPrev_l_, pointsPrev_r_;
+		// Points in 'current' images
+		std::vector<cv::Point2f> pointsCurr_l_;
+		
+		cv::Mat worldPoints, rVec, tVec;
+		
+		tf::TransformBroadcaster tfBr_	;
+		tf::Transform tfVehiOdom_, tfNewOld_;
+		
+		/* Constructor
+		 *
+		 * */
+		VisualOdometer(StereoSubscriber& stereoSub, std::string parentFrame, std::string childFrame) :
+			parentFrame_(parentFrame),
+			childFrame_(childFrame),
+			lastImgIdx_(0),
+			oFDDE_(3000),
+			bfMatcher_(cv::NORM_HAMMING, true)
 			{
+			stereoSub_ = &stereoSub;
+			tfVehiOdom_.setIdentity();
+			tfNewOld_.setIdentity();
+			}
+		
+		/* Fetches an stereo image pair from a sp_navigation::StereoSubscriber
+		 * returns true if a new pair of pictures was received and successfully updated
+		 * */
+		bool getImagePair()
+			{
+			if (!stereoSub_) throw std::runtime_error("Error in VisualOdometer::getImagePair: stereoSub points to no object!");
+			if (stereoSub_->imgConstPtr_l_ != NULL && stereoSub_->imgConstPtr_r_ != NULL)
+				{
+				if (imgCurr_l_.empty() || imgCurr_r_.empty())
+					{
+					stereoSub_->imgConstPtr_l_->image.copyTo(imgCurr_l_);
+					stereoSub_->imgConstPtr_r_->image.copyTo(imgCurr_r_);
+					lastImgIdx_ = stereoSub_->imgIdx;
+					std::cout << "Erstes Mal rein, kopiere nach imgPrev" << std::endl;
+					return false;
+					}
+				else if (stereoSub_->imgIdx != lastImgIdx_)
+					{
+					imgCurr_l_.copyTo(imgPrev_l_);
+					imgCurr_r_.copyTo(imgPrev_r_);
+					stereoSub_->imgConstPtr_l_->image.copyTo(imgCurr_l_);
+					stereoSub_->imgConstPtr_r_->image.copyTo(imgCurr_r_);
+					lastImgIdx_ = stereoSub_->imgIdx;
+					std::cout << "ImgCurr hat schon daten, rippele durch!" << std::endl;
+					return true;
+					}
+				}
+			else std::cout << "No images received!" << std::endl;
+			return false;
+			}
+		
+		/* Finds matches in the previous pair of stereo images
+		 *
+		 * */
+		bool findMatches()
+			{
+			// Check if there is a pair of images to find matches
+			if (imgPrev_l_.empty() || imgPrev_r_.empty() || imgCurr_l_.empty()) throw std::runtime_error("Error in VisualOdometer::updateStereoMatches(): No images to match!");
 			
+			// Clear previous found features
+			keyPointsPrev_l_.clear();
+			keyPointsPrev_r_.clear();
+			pointsPrev_l_.clear();
+			pointsPrev_r_.clear();
+			pointsCurr_l_.clear();
+			
+			// Detect features in left and right image of the previous pair
+			std::vector<cv::KeyPoint> keyPoints_l, keyPoints_r;
+			cv::Mat descriptors_l, descriptors_r;
+			oFDDE_(imgPrev_l_, cv::noArray(), keyPoints_l, descriptors_l);
+			oFDDE_(imgPrev_r_, cv::noArray(), keyPoints_r, descriptors_r);
+			
+			// Match features with a descriptor(!)-distance below a threshold
+			std::vector<std::vector<cv::DMatch> > matches;
+			bfMatcher_.radiusMatch(descriptors_l, descriptors_r, matches, 35);
+			
+			// Only use matches that fulfill the epipolar constraint, thus lying on a horizontal line
+			std::vector<cv::KeyPoint> refinedKPs_l, refinedKPs_r;
+			std::vector<cv::Point2f> refinedPoints_l, refinedPoints_r;
+			for(unsigned int i = 0; i < matches.size(); ++i)
+				{
+				if (matches[i].size() > 0 && fabs(keyPoints_l[matches[i][0].queryIdx].pt.y - keyPoints_r[matches[i][0].trainIdx].pt.y) <= 2)
+					{
+					refinedKPs_l.push_back(keyPoints_l[matches[i][0].queryIdx]);
+					refinedKPs_r.push_back(keyPoints_r[matches[i][0].trainIdx]);
+				
+					refinedPoints_l.push_back(keyPoints_l[matches[i][0].queryIdx].pt);
+					refinedPoints_r.push_back(keyPoints_r[matches[i][0].trainIdx].pt);
+					}
+				}
+					
+			// Remove outliers by RANSAC
+			std::vector<unsigned char> inlierStatus;
+			cv::findFundamentalMat(refinedPoints_l, refinedPoints_r, CV_FM_RANSAC, 3., 0.99, inlierStatus);
+			std::vector<cv::KeyPoint> stereoKPs_l, stereoKPs_r;
+			std::vector<cv::Point2f> stereoPoints_l, stereoPoints_r;
+			for(unsigned int i = 0; i < inlierStatus.size(); ++i)
+				{
+				if(inlierStatus[i])
+					{
+					stereoKPs_l.push_back(refinedKPs_l[i]);
+					stereoKPs_r.push_back(refinedKPs_r[i]);
+					
+					stereoPoints_l.push_back(refinedPoints_l[i]);
+					stereoPoints_r.push_back(refinedPoints_r[i]);
+					}
+				}
+			
+			// If there are no matches left, return false
+			if (stereoPoints_l.empty()) return false;
+			
+			// Try to find found features in the next image
+			std::vector<cv::Point2f> matchedPointsCurrentAll_l;
+			std::vector<unsigned char> opticalFlowStatus;
+			std::vector<float> opticalFlowError;
+			cv::calcOpticalFlowPyrLK(imgPrev_l_, imgCurr_l_, stereoPoints_l, matchedPointsCurrentAll_l, opticalFlowStatus, opticalFlowError);
+			
+			for(unsigned int i = 0; i < opticalFlowStatus.size(); i++)
+				{
+				if(opticalFlowStatus[i])
+					{
+					keyPointsPrev_l_.push_back(stereoKPs_l[i]);
+					keyPointsPrev_r_.push_back(stereoKPs_r[i]);
+					
+					pointsPrev_l_.push_back(stereoPoints_l[i]);
+					pointsPrev_r_.push_back(stereoPoints_r[i]);
+					
+					pointsCurr_l_.push_back(matchedPointsCurrentAll_l[i]);
+					}
+				}
+			
+			if (pointsCurr_l_.size() > 5) return true;
+			else return false;
+			}
+			
+		/* Calculates pose between current and last camera pose after getting new images and matching features
+		 *
+		 * */
+		void updateRT()
+			{
+			// If images successfully updated and features matched, update tfVehiOdom with new tf, otherwise just last transform
+			if (getImagePair() && findMatches())
+				{
+				const cv::Mat projectionMatrix_l(stereoSub_->stCamModel_.left().projectionMatrix());
+				cv::triangulatePoints(projectionMatrix_l, stereoSub_->stCamModel_.right().projectionMatrix(), pointsPrev_l_, pointsPrev_r_, worldPoints);
+				
+				cv::solvePnPRansac(worldPoints.rowRange(0, 3).t(), pointsCurr_l_, projectionMatrix_l.colRange(0, 3), cv::noArray(), rVec, tVec, false, 500, 2.0);
+			
+				cv::Mat R;
+				cv::Rodrigues(rVec, R);
+				tf::Matrix3x3 rRos(R.at<double>(0,0), R.at<double>(0,1), R.at<double>(0,2),
+									R.at<double>(1,0), R.at<double>(1,1), R.at<double>(1,2),
+									R.at<double>(2,0), R.at<double>(2,1), R.at<double>(2,2));
+				tfNewOld_ = tf::Transform(rRos, tf::Vector3(tVec.at<double>(0), tVec.at<double>(1), tVec.at<double>(2)));
+				}
+			tfVehiOdom_ *= tfNewOld_;
+			}
+		
+		/* Publishes the last found rotation and translation
+		 *
+		 * */
+		void publishTF()
+			{
+			geometry_msgs::TransformStamped tfMessage;
+			tfMessage.header.stamp = ros::Time::now();
+			tfMessage.header.frame_id = parentFrame_;
+			tfMessage.child_frame_id = childFrame_;
+			tfMessage.transform.translation.x = tfVehiOdom_.getOrigin().getX();
+			tfMessage.transform.translation.y = tfVehiOdom_.getOrigin().getY();
+			tfMessage.transform.translation.z = tfVehiOdom_.getOrigin().getZ();
+			tfMessage.transform.rotation.x = tfVehiOdom_.getRotation().getX();
+			tfMessage.transform.rotation.y = tfVehiOdom_.getRotation().getY();
+			tfMessage.transform.rotation.z = tfVehiOdom_.getRotation().getZ();
+			tfMessage.transform.rotation.w = tfVehiOdom_.getRotation().getW();
+			tfBr_.sendTransform(tfMessage);
 			}
 	};
 
@@ -116,172 +316,48 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "sp_navigation");
 	ros::NodeHandle nh;
 	sp_navigation::StereoSubscriber stereoSub(nh, "stereo");
-
-	cv::Mat left, right, new_l, new_r;
-	cv::ORB oFDDE(3000);	// ORB feature detector and descriptor extractor
-	cv::BFMatcher bfMatcher(cv::NORM_HAMMING, true); // Bruteforce matcher with implemented cross-checking if second arg is 'true'
-	
-	tf::TransformBroadcaster tfBr;
-	tf::Transform tfVehiOdom;
-	tfVehiOdom.setIdentity();
+	sp_navigation::VisualOdometer visualOdo(stereoSub, std::string("odom"), std::string("base_link"));
 	
 	ros::Rate loopRate(12);
 	while (ros::ok())
 		{
 		// Take time of each loop
 		double tickTime = (double)cv::getTickCount();
-		// Let ROS spin, callback methods are called
 		ros::spinOnce();
-		if (stereoSub.imgConstPtr_l_ != NULL && stereoSub.imgConstPtr_r_ != NULL)
+		
+		try
 			{
-			// Copy new pictures
-			if (new_l.empty() || new_r.empty())
+			visualOdo.updateRT();
+			std::cout << "Featuresuche erfogreich." << std::endl;
+			std::cout << "P links  : " << visualOdo.pointsPrev_l_.size() << std::endl;
+			std::cout << "P rechts : " << visualOdo.pointsPrev_r_.size() << std::endl;
+			std::cout << "P curr_l : " << visualOdo.pointsCurr_l_.size() << std::endl;
+/*			
+			for (unsigned int i = 0; i < visualOdo.stereoPoints_l.size(); ++i)
 				{
-				stereoSub.imgConstPtr_l_->image.copyTo(new_l);
-				stereoSub.imgConstPtr_r_->image.copyTo(new_r);
-				continue;
+				cv::circle(visualOdo.imgPrev_l_, visualOdo.stereoPoints_l[i], 5, cv::Scalar(200,0,0));
+				cv::circle(visualOdo.imgPrev_r_, visualOdo.stereoPoints_r[i], 5, cv::Scalar(200,0,0));
 				}
-			new_l.copyTo(left);
-			new_r.copyTo(right);
-			stereoSub.imgConstPtr_l_->image.copyTo(new_l);
-			stereoSub.imgConstPtr_r_->image.copyTo(new_r);
 			
-			// Detect features and compute descriptors
-			std::vector<cv::KeyPoint> keyPoints_l, keyPoints_r;
-			cv::Mat descriptors_l, descriptors_r;
-			oFDDE(left, cv::noArray(), keyPoints_l, descriptors_l);
-			oFDDE(right, cv::noArray(), keyPoints_r, descriptors_r);
-			std::cout << "KeyPoints left:		" << keyPoints_l.size() << std::endl;
-			std::cout << "KeyPoints right:	" << keyPoints_r.size() << std::endl;
-			cv::drawKeypoints(left, keyPoints_l, left, cv::Scalar(255,0,0));
-			
-			// Match features with a descriptor(!)-distance below a threshold
-			std::vector<std::vector<cv::DMatch> > matches;
-			bfMatcher.radiusMatch(descriptors_l, descriptors_r, matches, 35);
-			
-			// Only use matches that fulfill the epipolar constraint, thus lying on one horizontal line
-			unsigned int numberOfMatches = 0;
-			std::vector<cv::DMatch> refinedMatches;
-			for(unsigned int i = 0; i < matches.size(); ++i)
+			cv::drawKeypoints(visualOdo.imgPrev_l_, visualOdo.stereoKPs_l, visualOdo.imgPrev_l_, cv::Scalar(0,0,200));
+			cv::drawKeypoints(visualOdo.imgPrev_r_, visualOdo.stereoKPs_r, visualOdo.imgPrev_r_, cv::Scalar(0,0,200));
+*/			
+			for (unsigned int i = 0; i < visualOdo.pointsPrev_l_.size(); ++i)
 				{
-				numberOfMatches += matches[i].size() > 0 ? 1 : 0;
-				if (matches[i].size() > 0 && fabs(keyPoints_l[matches[i][0].queryIdx].pt.y - keyPoints_r[matches[i][0].trainIdx].pt.y) <= 2)
-					{
-					refinedMatches.push_back(matches[i][0]);
-					cv::line(left, cv::Point(keyPoints_l[matches[i][0].queryIdx].pt.x, keyPoints_l[matches[i][0].queryIdx].pt.y), cv::Point(keyPoints_r[matches[i][0].trainIdx].pt.x, keyPoints_r[matches[i][0].trainIdx].pt.y), cv::Scalar(0,0,200));
-					}
-				}
-			std::cout << "Matches:		" << numberOfMatches << std::endl;
-			std::cout << "refined Matches:	" << refinedMatches.size() << std::endl;
-			// Create Vectors containing only Points from refined matches
-			std::vector<cv::KeyPoint> refinedKeyPoints_l, refinedKeyPoints_r;
-			std::vector<cv::Point2f> refinedPoints_l, refinedPoints_r;
-			for (unsigned int i = 0; i < refinedMatches.size(); ++i)
-				{
-				refinedKeyPoints_l.push_back(keyPoints_l[refinedMatches[i].queryIdx]);
-				refinedKeyPoints_r.push_back(keyPoints_r[refinedMatches[i].trainIdx]);
-				
-				refinedPoints_l.push_back(keyPoints_l[refinedMatches[i].queryIdx].pt);
-				refinedPoints_r.push_back(keyPoints_r[refinedMatches[i].trainIdx].pt);
-				}
-			cv::drawKeypoints(left, refinedKeyPoints_l, left, cv::Scalar(0, 255, 0));
-			
-			// Remove outliers by RANSAC
-			std::vector<unsigned char> inlierStatus;
-			cv::findFundamentalMat(refinedPoints_l, refinedPoints_r, CV_FM_RANSAC, 3., 0.99, inlierStatus);
-			// Create Vectors containing only Points that have been proven consistent by RANSAC
-			std::vector<cv::KeyPoint> ransacProofKPs_l, ransacProofKPs_r;
-			std::vector<cv::Point2f> ransacProofPoints_l, ransacProofPoints_r;
-			for(unsigned int i = 0; i < inlierStatus.size(); ++i)
-				{
-				if(inlierStatus[i])
-					{
-					ransacProofKPs_l.push_back(refinedKeyPoints_l[i]);
-					ransacProofKPs_r.push_back(refinedKeyPoints_r[i]);
-					
-					ransacProofPoints_l.push_back(refinedPoints_l[i]);
-					ransacProofPoints_r.push_back(refinedPoints_r[i]);
-					}
-				}
-			std::cout << "RANSAC proof KPs: 	" << ransacProofKPs_l.size() << std::endl;
-			
-			// Track features in next set of stereo image
-			std::vector<cv::Point2f> matchedPointsNewAll_l;
-			std::vector<unsigned char> opticalFlowStatus;
-			std::vector<float> opticalFlowError;
-			try
-				{
-				cv::calcOpticalFlowPyrLK(left, new_l, ransacProofPoints_l, matchedPointsNewAll_l, opticalFlowStatus, opticalFlowError);
-				}
-			catch(cv::Exception& e)
-				{
-				std::cout << e.what() << std::endl;
-				}
-			std::vector<cv::Point2f> twiceMatchedPoints_l, twiceMatchedPoints_r, matchedPointsNew_l;
-			std::vector<cv::KeyPoint> twiceMatchedKPs_l, twiceMatchedKPs_r;
-			for(unsigned int i = 0; i < opticalFlowStatus.size(); i++)
-				{
-				if(opticalFlowStatus[i])
-					{
-					twiceMatchedPoints_l.push_back(ransacProofPoints_l[i]);
-					twiceMatchedPoints_r.push_back(ransacProofPoints_r[i]);
-					
-					twiceMatchedKPs_l.push_back(ransacProofKPs_l[i]);
-					twiceMatchedKPs_r.push_back(ransacProofKPs_r[i]);
-					
-					matchedPointsNew_l.push_back(matchedPointsNewAll_l[i]);
-					}
-				}
-			std::cout << "Twice matched KPs:	" << twiceMatchedKPs_l.size() << std::endl;
-			std::cout << "Points in new img:	" << matchedPointsNew_l.size() << std::endl;
-			
-			// Here comes the Odometry part
-			// First try to triangulate the points, then get transform between camera poses and publish it
-			cv::Mat worldPoints, rVec, tVec;
-			const cv::Mat projectionMatrix(stereoSub.stCamModel_.left().projectionMatrix());
-			try
-				{
-				cv::triangulatePoints(projectionMatrix, stereoSub.stCamModel_.right().projectionMatrix(), twiceMatchedPoints_l, twiceMatchedPoints_r, worldPoints);
-					
-				cv::solvePnPRansac(worldPoints.rowRange(0, 3).t(), matchedPointsNew_l, projectionMatrix.colRange(0, 3), cv::noArray(), rVec, tVec, false, 500, 2.0);
-				
-				cv::Mat R;
-				cv::Rodrigues(rVec, R);
-				tf::Matrix3x3 rRos(R.at<double>(0,0), R.at<double>(0,1), R.at<double>(0,2),
-									R.at<double>(1,0), R.at<double>(1,1), R.at<double>(1,2),
-									R.at<double>(2,0), R.at<double>(2,1), R.at<double>(2,2));
-				tf::Transform tfNewOld(rRos, tf::Vector3(tVec.at<double>(0), tVec.at<double>(1), tVec.at<double>(2)));
-				tfVehiOdom *= tfNewOld;
-				}
-			catch(cv::Exception& e)
-				{
-				std::cout << e.what() << std::endl;
-				}
-			catch(...)
-				{
-				std::cout << "Something went wrong in the odomodong" << std::endl;
+				cv::circle(visualOdo.imgPrev_l_, visualOdo.pointsPrev_l_[i], 5, cv::Scalar(0,200,0));
+				cv::circle(visualOdo.imgCurr_l_, visualOdo.pointsCurr_l_[i], 5, cv::Scalar(0,200,0));
 				}
 			
 			
-			// Draw Matches
-			cv::Mat imgMatches;
-			cv::drawMatches(left, keyPoints_l, right, keyPoints_r, refinedMatches, imgMatches);
-			// Draw RANSAC proof KeyPoints in red
-			cv::drawKeypoints(left, ransacProofKPs_l, left, cv::Scalar(0, 0, 200));
-			
-			geometry_msgs::TransformStamped tfMessage;
-			tfMessage.header.stamp = ros::Time::now();
-			tfMessage.header.frame_id = "odom";
-			tfMessage.child_frame_id = "base_link";
-			tfMessage.transform.translation.x = tfVehiOdom.getOrigin().getX()/1000;
-			tfMessage.transform.translation.y = tfVehiOdom.getOrigin().getY()/1000;
-			tfMessage.transform.translation.z = tfVehiOdom.getOrigin().getZ()/1000;
-			tfMessage.transform.rotation.x = tfVehiOdom.getRotation().getX();
-			tfMessage.transform.rotation.y = tfVehiOdom.getRotation().getY();
-			tfMessage.transform.rotation.z = tfVehiOdom.getRotation().getZ();
-			tfMessage.transform.rotation.w = tfVehiOdom.getRotation().getW();
-			tfBr.sendTransform(tfMessage);
-			stereoSub.publishCVImages(left, imgMatches);
+			stereoSub.publishCVImages(visualOdo.imgPrev_l_, visualOdo.imgCurr_l_);
+			}
+		catch(std::exception& e)
+			{
+			std::cout << e.what() << std::endl;
+			}
+		catch(...)
+			{
+			std::cout << "Something unknown went wrong." << std::endl;
 			}
 		
 		tickTime = ((double)cv::getTickCount() - tickTime) / cv::getTickFrequency();
